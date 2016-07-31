e0292c2c9042576af82ab6d6a79bb1ad915a6e35
darthVader <krishoza15sep@gmail.com>
2016-07-31 22:15:05 +0530

    partial implementation of prototype patter using virtual constructor

---
+++ cpp_programs/virtual_copy_constructor_2.cxx
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+	public:
+		Base()
+		{
+		}
+
+		virtual ~Base() //ensures to invoke actual object destructor
+		{
+		}
+
+		virtual void changeAttributes() = 0;
+
+		//the virtual constructor
+		static Base* create(int id);
+
+		//the virtual copy constructor
+		virtual Base* clone() = 0;
+
+};
+
+class Derived1 : public Base
+{
+	public:
+		Derived1()
+		{
+			cout << "Derived1 created" << endl;
+		}
+
+
+
